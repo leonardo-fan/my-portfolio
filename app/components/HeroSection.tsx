914d8647e4ebeb2bd7aa17ecbd6f25007627a808
Leonardo Fan <leonardofan2000@gmail.com>
2024-01-04 23:13:24 +1100

    hero section

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,10 @@
+export default function HeroSection() {
+  return (
+    <section id="hero" className="flex items-center h-screen">
+      <div>
+        <div className="font-bold text-8xl py-1">Leonardo Fan</div>
+        <div className="text-6xl py-1">Software Engineer</div>
+      </div>
+    </section>
+  )
+}
