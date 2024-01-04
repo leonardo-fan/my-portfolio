9539b4bde58c8e966333ac4afb59b04f58301565
Leonardo Fan <leonardofan2000@gmail.com>
2024-01-04 23:13:56 +1100

    structure of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-
+import HeroSection from "./components/HeroSection"
+import AboutSection from "./components/AboutSection"
+import ExperienceSection from "./components/ExperienceSection"
+import ProjectsSection from "./components/ProjectsSection"
 
 export default function Home() {
   return (
-    <section>
-      <div className="h-screen">
-        hi
-      </div>
-    </section>
+    <div className="w-screen px-4 lg:px-6 mx-auto max-w-screen-xl">
+      <HeroSection />
+      <AboutSection />
+      <ExperienceSection />
+      <ProjectsSection />
+    </div>
   )
 }
