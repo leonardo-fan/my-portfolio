9301564b2431a2a029d2d43f51426a5616b2821d
Leonardo Fan <leonardofan2000@gmail.com>
2024-01-05 12:03:24 +1100

    responsive kinda

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -6,8 +6,8 @@ export default function ProjectsSection() {
     <section id="projects" className="flex items-center h-screen">
       <div>
         <Title text="Projects" level={1} />
-        <div id="project-1" className="flex flex-row items-center justify-between">
-          <div id="project-1-desc" className="pr-5">
+        <div id="project-1" className="flex sm:flex-col md:flex-row items-center justify-between">
+          <div id="project-1-desc" className="pr-5 sm:w-screen md:w-fit">
             <Title text="UNSW Circles" level={2} />
             <p>Led an agile team of 10 students, supported by Git and CI/CD pipelines, to build a degree planner for UNSW. The front-end was built with React and TypeScript, while the backend uses Python and FastAPI. Currently used by many students with ~8500 total visits per month.</p>
             <div id="project-1-links" className="mt-5">
